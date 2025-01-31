aa5d164bfce15edee9ea6e681cd147d0923760a0
AdarshFiveonline <helpdesk@fiveonline.in>
2025-01-31 18:49:10 +0530

    added new page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,90 +1,90 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <section className="home-banner w-100 overflow-hidden">
-      <div className="row">
-        <div className="video-banner">
-          <video
-            src="/videos/5726501-uhd_3840_2160_30fps.mp4"
-            loop
-            muted
-            autoPlay
-            className="w-100"
-          ></video>
+    <>
+      <Header />
+      <section className="home-banner w-100 overflow-hidden">
+        <div className="row">
+          <div className="video-banner">
+            <video
+              src="/videos/5726501-uhd_3840_2160_30fps.mp4"
+              loop
+              muted
+              autoPlay
+              className="w-100"
+            ></video>
 
-          <div className="home-banner-content">
-            <div className="container">
-              <h1 className="section-title">
-                Innovative investments. <br /> Empowering growth.
-              </h1>
-              <div className="im-a-content-div">
-                <h5>I am a ...</h5>
-              </div>
-              <div className="banner-contents">
-                <div className="row">
-                  <div className="col-lg-12">
-                    <div className="row align-items-center">
-                      <div className="col-lg-4">
-                        <a href="#" className="content-div">
-                          <div className="content-div-title">
-                            <h2>
-                              Corporate, Family Office & Individual Investor
-                            </h2>
-                            <i className="fa-solid fa-arrow-right"></i>
-                          </div>
+            <div className="home-banner-content">
+              <div className="container">
+                <h1 className="section-title">
+                  Investing in future. <br /> Backing emerging companies.
+                </h1>
+                <div className="im-a-content-div">
+                  <h5>I am a ...</h5>
+                </div>
+                <div className="banner-contents">
+                  <div className="row">
+                    <div className="col-lg-12">
+                      <div className="row align-items-center">
+                        <div className="col-lg-4">
+                          <a href="#" className="content-div">
+                            <div className="content-div-title">
+                              <h2>Foreign Investor</h2>
+                              <i className="fa-solid fa-arrow-right"></i>
+                            </div>
 
-                          <div className="content-para">
-                            <div className="content-title">
-                              <h5>
-                                Corporate, Family Office & Individual Investor
-                              </h5>
+                            <div className="content-para">
+                              <div className="content-title">
+                                <h5>Foreign Investor</h5>
+                                <i className="fa-solid fa-arrow-right"></i>
+                              </div>
+                              <p>
+                                As a founder-led company, we foster innovative
+                                ideas to build scalable businesses and personal
+                                legacies that have a collective global impact.
+                              </p>
+                            </div>
+                          </a>
+                        </div>
+                        <div className="col-lg-4">
+                          <a href="#" className="content-div">
+                            <div className="content-div-title">
+                              <h2>NRI Investor</h2>
                               <i className="fa-solid fa-arrow-right"></i>
                             </div>
-                            <p>
-                              As a founder-led company, we foster innovative
-                              ideas to build scalable businesses and personal
-                              legacies that have a collective global impact.
-                            </p>
-                          </div>
-                        </a>
-                      </div>
-                      <div className="col-lg-4">
-                        <a href="#" className="content-div">
-                          <div className="content-div-title">
-                            <h2>Institutional Investor</h2>
-                            <i className="fa-solid fa-arrow-right"></i>
-                          </div>
-                          <div className="content-para">
-                            <div className="content-title">
-                              <h5>Institutional Investor</h5>
+                            <div className="content-para">
+                              <div className="content-title">
+                                <h5>NRI Investor</h5>
+                                <i className="fa-solid fa-arrow-right"></i>
+                              </div>
+                              <p>
+                                As a founder-led company, we foster innovative
+                                ideas to build scalable businesses and personal
+                                legacies that have a collective global impact.
+                              </p>
+                            </div>
+                          </a>
+                        </div>
+                        <div className="col-lg-4">
+                          <a href="#" className="content-div">
+                            <div className="content-div-title">
+                              <h2>Startup Founder & Entrepreneur</h2>
                               <i className="fa-solid fa-arrow-right"></i>
                             </div>
-                            <p>
-                              As a founder-led company, we foster innovative
-                              ideas to build scalable businesses and personal
-                              legacies that have a collective global impact.
-                            </p>
-                          </div>
-                        </a>
-                      </div>
-                      <div className="col-lg-4">
-                        <a href="#" className="content-div">
-                          <div className="content-div-title">
-                            <h2>Startup Founder & Entrepreneur</h2>
-                            <i className="fa-solid fa-arrow-right"></i>
-                          </div>
-                          <div className="content-para">
-                            <div className="content-title">
-                              <h5>Startup Founder & Entrepreneur</h5>
-                              <i className="fa-solid fa-arrow-right"></i>
+                            <div className="content-para">
+                              <div className="content-title">
+                                <h5>Startup Founder & Entrepreneur</h5>
+                                <i className="fa-solid fa-arrow-right"></i>
+                              </div>
+                              <p>
+                                We leverage our intellectual capital and
+                                resources to enhance entrepreneurial ecosystems.
+                              </p>
                             </div>
-                            <p>
-                              We leverage our intellectual capital and resources
-                              to enhance entrepreneurial ecosystems.
-                            </p>
-                          </div>
-                        </a>
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -93,8 +93,8 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
